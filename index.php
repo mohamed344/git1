90eb22ad5153e52ae4bd3a8675eae7914824e080
Mehdi <charbouni.mehdi@hotmail.fr>
2020-01-20 14:00:09 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title> Hello Application</title>
+</head>
+<body>
+
+<?php
+// Simple greeting message
+echo "Hello, Ahmed! from Mehdi";
+?>
+
+</body>
+</html>                                		
